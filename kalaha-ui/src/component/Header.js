64ef85d3d225c72870608ea04fa80b1c8f30c0b8
pradeep swamireddy <pradeepntg@live.com>
2021-08-15 14:59:41 +0100

    react-router-dom dependency and mancala icon to title and NavBar

--- kalaha-ui/src/component/Header.js
+++ kalaha-ui/src/component/Header.js
@@ -1,16 +1,17 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import icon from '../icons/mancala.png';
 
 function Header() {
 return(
 <Navbar bg="primary" variant="dark">
 <Container>
-<Navbar.Brand href="#home">Kalaha</Navbar.Brand>
+<Navbar.Brand href="#home">
+  <img src={icon} width="30" height="30" class="d-inline-block align-top" alt="" />
+  Kalaha/Mancala</Navbar.Brand>
 <Nav className="me-auto">
-  <Nav.Link href="#home">Home</Nav.Link>
-  <Nav.Link href="#features">Features</Nav.Link>
-  <Nav.Link href="#pricing">Pricing</Nav.Link>
+  <Nav.Link href="https://www.wikihow.com/Play-Kalaha">How To Play</Nav.Link>  
 </Nav>
 </Container>
 </Navbar>
